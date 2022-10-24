5f8b849b3d2afb998cd9ea7860855e9bdfbe0680
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-24 08:14:41 +0000

    show navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const Home = () => {
   return (
     <section>
+      <Navbar />
       <div className="">
         <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-900">
           <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-900">
